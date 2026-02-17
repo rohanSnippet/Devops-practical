f45bdad62b3c5567a05cbc0ccee5ed79222558b7
Rohan M <rohan110620@gmail.com>
2026-02-17 12:23:20 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,16 @@
+package factorial;
+
+public class Program1 {
+	
+	    public int calculate(int n) {
+	    	if(n == 0) return 1;
+	    	return n * calculate(n - 1);
+	    }
+        public static void main(String []args) {
+        	Program1 fact = new Program1();
+            int number = 6;
+            int result = fact.calculate(number);
+            
+            System.out.println("The factorial of " + number + " is: " + result);
+        }
+} 
